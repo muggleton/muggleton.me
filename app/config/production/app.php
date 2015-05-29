ebdcc3560e429905ef10dab8ac6de837fbd4a30c
David Muggleton <david@vibesfamily.com>
2015-05-29 13:47:46 +0100

    Modify config location for security

--- app/config/production/app.php
+++ app/config/production/app.php
@@ -1,9 +1,3 @@
 <?php
 // Require private configuration so cannot be viewed in public respitory
-require_once('/var/www/muggleton.me/private.php');
-
-return array(
-	'debug' => false,
-	'url' => 'http://muggleton.me',
-	'key' => $private['app']['key'],
-	);
+require_once('/var/www/muggleton.me/private/app.php');
