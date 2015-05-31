3af7728b49751cf13007f1e2da65cf8840247f81
David Muggleton <david@vibesfamily.com>
2015-05-31 18:03:28 +0100

    Modify user table seeder to include external class hidden from public view

--- app/database/seeds/UserTableSeeder.php
+++ app/database/seeds/UserTableSeeder.php
@@ -1,11 +1,2 @@
 <?php
-class UserTableSeeder extends Seeder {
-
-	public function run()
-	{
-		DB::table('users')->delete();
-
-		User::create(['username' => 'david', 'email' => 'david@muggleton.me', 'password' => Hash::make('hello')]);
-	}
-
-}
+return include '/var/www/muggleton.me/private/UserTableSeeder.php';
