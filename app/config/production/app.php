098912910f7137599784209c29a53de42995293d
David Muggleton <david@vibesfamily.com>
2015-05-31 17:52:38 +0100

    Change include function to array is returned correctly for configuration

--- app/config/production/app.php
+++ app/config/production/app.php
@@ -1,3 +1,3 @@
 <?php
 // Require private configuration so cannot be viewed in public respitory
-require_once('/var/www/muggleton.me/private/app.php');
+return include '/var/www/muggleton.me/private/app.php';
--- app/config/production/database.php
+++ app/config/production/database.php
@@ -1,3 +1,3 @@
 <?php
 // Require private configuration so cannot be viewed in public respitory
-require_once('/var/www/muggleton.me/private/database.php');
+return include '/var/www/muggleton.me/private/database.php';
