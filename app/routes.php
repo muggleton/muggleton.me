f13981bbf3dcacf58e0354f52cd581511aae5083
David Muggleton <david@vibesfamily.com>
2015-06-01 10:00:56 +0100

    Change route order

--- app/routes.php
+++ app/routes.php
@@ -1,8 +1,7 @@
 <?php
-// Overide project name wild card
 Route::get('/projects/new', 'ProjectController@getNew');
-Route::controller('/projects', 'ProjectController');
 Route::get('/projects/{project_name}', 'ProjectController@getView')->where('project_name', '[a-zA-Z-_]+');
+Route::controller('/projects', 'ProjectController');
 
 Route::controller('/contact', 'ContactController');
 Route::controller('/admin', 'AdminController');
