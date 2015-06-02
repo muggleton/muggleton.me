4e047cc41563dfac939a7dbc7b4fbd735fb39391
David Muggleton <david@vibesfamily.com>
2015-06-02 15:07:07 +0100

    Fix environmet

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -26,7 +26,7 @@ $app = new Illuminate\Foundation\Application;
 
 $env = $app->detectEnvironment(array(
 
-	'local' => array('muggleton-local'),
+	'local' => 	['muggleton-local'],
 	'production' => ['muggleton-www-production']
 
 	));
